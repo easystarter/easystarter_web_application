68f73bc707af2dbae51e297b86ff87a175dbd971
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-04-14 20:26:52 +0300

    [CREATE] idea_storage_app created

--- easystarter_web/settings.py
+++ easystarter_web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'idea_storage_app',
 ]
 
 MIDDLEWARE = [
---
+++ idea_storage_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IdeaStorageAppConfig(AppConfig):
+    name = 'idea_storage_app'
