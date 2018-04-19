dd9bc9a9b036631e6635d3fe1190940d82418f0f
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-04-19 23:48:06 +0300

    [UPDATE] goal, pledge, backers_counter, day_to_go fields in Concept model added, concept templates modified

--- concepts_storage_app/models.py
+++ concepts_storage_app/models.py
@@ -7,11 +7,16 @@ from markdown import markdown
 
 
 class Concept(models.Model):
+    SHORT_TERM = '30'
+    MIDDLE_TERM = '45'
+    LONG_TERM = '60'
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     pub_date = models.DateTimeField('date published')
+
 
     def was_published_recently(self):
         now = timezone.now()
@@ -23,7 +28,18 @@ class Concept(models.Model):
     def __str__(self):
         return self.title
 
+    goal = models.IntegerField(default=0)
+    pledge = models.IntegerField(default=0)
 
+    def funded(self):
+        return (self.pledge/self.goal)*100
+
+    backers_counter = models.IntegerField(default=0)
+    day_to_go = models.CharField(max_length=100, choices=(
+        (SHORT_TERM, '30 days to go, platform fee is 5% of the project cost'),
+        (MIDDLE_TERM, '45 days to go, platform fee is 10% of the project cost'),
+        (LONG_TERM, '60 days to go, platform fee is 15% of the project cost')),
+        default=SHORT_TERM)
 
 #     markdown_description_field = models.TextField()
 #     html_description_field = models.TextField(editable=False)
